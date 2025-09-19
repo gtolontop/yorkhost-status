d42b1bd42df84a8976c15298fc98106fba2a7f40
Cruck Store <cruckstore+github@gmail.com>
2025-09-19 20:55:33 +0200

    Refactor styles to use CSS variables from globals.css and remove global imports

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
